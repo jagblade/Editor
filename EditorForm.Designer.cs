090e149f430f5cb6b4dbdf5308d3b9d6819b8db5
jagblade <jagblade@ABV.BG>
2014-06-29 03:53:45 +0300

    Upload

---
+++ EditorForm.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace Editor
+{
+    partial class EditorForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            this.txtAuthor = new System.Windows.Forms.TextBox();
+            this.lblContent = new System.Windows.Forms.Label();
+            this.txtContent = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 500);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(129, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load Document";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(147, 500);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(129, 23);
+            this.btnSave.TabIndex = 1;
+            this.btnSave.Text = "Save Document";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(69, 12);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(382, 22);
+            this.txtTitle.TabIndex = 2;
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Location = new System.Drawing.Point(13, 13);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(35, 17);
+            this.lblTitle.TabIndex = 3;
+            this.lblTitle.Text = "Title";
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.AutoSize = true;
+            this.lblAuthor.Location = new System.Drawing.Point(13, 50);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(50, 17);
+            this.lblAuthor.TabIndex = 5;
+            this.lblAuthor.Text = "Author";
+            // 
+            // txtAuthor
+            // 
+            this.txtAuthor.Location = new System.Drawing.Point(69, 49);
+            this.txtAuthor.Name = "txtAuthor";
+            this.txtAuthor.Size = new System.Drawing.Size(382, 22);
+            this.txtAuthor.TabIndex = 4;
+            // 
+            // lblContent
+            // 
+            this.lblContent.AutoSize = true;
+            this.lblContent.Location = new System.Drawing.Point(13, 86);
+            this.lblContent.Name = "lblContent";
+            this.lblContent.Size = new System.Drawing.Size(57, 17);
+            this.lblContent.TabIndex = 6;
+            this.lblContent.Text = "Content";
+            // 
+            // txtContent
+            // 
+            this.txtContent.Location = new System.Drawing.Point(16, 107);
+            this.txtContent.Multiline = true;
+            this.txtContent.Name = "txtContent";
+            this.txtContent.Size = new System.Drawing.Size(435, 387);
+            this.txtContent.TabIndex = 7;
+            // 
+            // EditorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(463, 535);
+            this.Controls.Add(this.txtContent);
+            this.Controls.Add(this.lblContent);
+            this.Controls.Add(this.lblAuthor);
+            this.Controls.Add(this.txtAuthor);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnLoad);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "EditorForm";
+            this.Text = "Editor";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblAuthor;
+        private System.Windows.Forms.TextBox txtAuthor;
+        private System.Windows.Forms.Label lblContent;
+        private System.Windows.Forms.TextBox txtContent;
+    }
+}
+
---
+++ EditorForm.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Serialization;
+
+namespace Editor
+{
+    public partial class EditorForm : Form
+    {
+        public EditorForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Editor files | *.edt;*.xedt";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Document doc = null;
+                    using (Stream file = dialog.OpenFile())
+                    {
+                        if (dialog.FileName.ToLower().EndsWith(".edt"))
+                        {
+                            BinaryFormatter formatter = new BinaryFormatter();
+                            doc = (Document)formatter.Deserialize(file);
+                        }
+                        else if (dialog.FileName.ToLower().EndsWith(".xedt"))
+                        {
+                            XmlSerializer serializer = new XmlSerializer(typeof(Document));
+                            doc = (Document)serializer.Deserialize(file);
+                        }
+                        else
+                        {
+                            throw new InvalidOperationException("Unexpected file type");
+                        }
+                    }
+
+                    txtTitle.Text = doc.Title;
+                    txtAuthor.Text = doc.Author;
+                    txtContent.Text = doc.Content;
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Cannot load file");
+                }
+            }
+        }
+
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrWhiteSpace(txtTitle.Text))
+            {
+                MessageBox.Show("Missing title");
+                return;
+            }
+
+            if (String.IsNullOrWhiteSpace(txtAuthor.Text))
+            {
+                MessageBox.Show("Missing author");
+                return;
+            }
+
+            SaveFileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Editor files | *.edt;*.xedt";
+
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                using (Stream file = dialog.OpenFile())
+                {
+                    Document doc = new Document();
+                    doc.Author = txtAuthor.Text.Trim();
+                    doc.Title = txtTitle.Text.Trim();
+                    doc.Content = txtContent.Text;
+
+                    if (dialog.FileName.ToLower().EndsWith(".edt"))
+                    {
+                        BinaryFormatter formatter = new BinaryFormatter();
+                        formatter.Serialize(file, doc);
+                    }
+                    else if (dialog.FileName.ToLower().EndsWith(".xedt"))
+                    {
+                        XmlSerializer serializer = new XmlSerializer(typeof(Document));
+                        serializer.Serialize(file, doc);
+                    }
+                }
+            }
+        }
+
+    }
+}
